e7def8b9b71a660e0176535e8d30774b7f8a560b
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-10-18 14:56:32 +0300

    Hata Yönetimi II

---
+++ II.Week /excepotional II.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Exceptions
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Yaşınızı giriniz:");
+            byte yas = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Yaşınız başarılı bir şekilde sisteme aktarıldı !\nYaşınız: {0}", yas);
+
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Byte türünün dışında farklı bir karakter girdiniz bunu için lütfen doğru karakteri giriniz ! ");
+        }
+
+        catch (OverflowException ex)
+        {
+            Console.WriteLine("Byte türünün dışında farklı bir uzunlukta sayı girdiniz !");
+        }
+
+        catch (Exception)
+        {
+            Console.WriteLine("Farklı bir hata tespit edildi !");
+        }
+
+    }
+
+}
