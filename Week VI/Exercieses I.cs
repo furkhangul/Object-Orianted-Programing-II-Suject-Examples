ff690974863170e15c9f8a7f623a70c9a4d9f60e
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-10-25 14:52:43 +0300

    1. Egzersiz

---
+++ Week VI/Exercieses I.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+class Animal
+{
+    private string Name { get; set; }
+    private int Id {  get; set; }
+    private int Age {  get; set; }
+
+    void setName()
+    {
+
+    }
+}
